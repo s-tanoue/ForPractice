8539b68c3fd79bf6c80e76f6683acb647803a2c2
藤田 <44770585+vFujitaMasahiro@users.noreply.github.com>
2019-12-11 14:30:59 +0900

    修正

--- ex1.c
+++ ex1.c
@@ -2,21 +2,25 @@
 
 int main()
 {
-	int a,b,ans;
-//こんにちは
+	int a, b, ans;
+	//こんにちは
 	printf("aを入力してください:");
-    scanf("%d",&a);
-    printf("bを入力してください:");
-    scanf("%d",&b);
-    
-	if( a == 2) {
+	scanf("%d", &a);
+	printf("bを入力してください:");
+	scanf("%d", &b);
+
+	if (a == 2)
+	{
 		ans = a * b;
-	}else{
-		ans = a + b ;
+	}
+	else
+	{
+		ans = a + b;
 		ans = ans * 10;
 	}
-	printf(" %d %d \n " ,a,b );
-    printf("----------------%d-------------- \n" ,ans);
+	printf(" %d %d \n ", a, b);
+	printf("----------------%d-------------- \n", ans);
 	// テスト（岩瀬）
 	// テスト（田上）
+	// テスト（偽田上）
 }
